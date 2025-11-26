da543543fae290e688cb1ba74250e5c671832b0d
David Ezekiel <david@axumtechnologies.com>
2025-11-26 15:44:17 +0100

    error fix

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import { SignupForm } from '@/features/auth/components/SignupForm';
-import Image from 'next/image';
 import Link from 'next/link';
 
 export const metadata: Metadata = {
